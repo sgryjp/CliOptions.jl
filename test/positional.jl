515697e68ed50a4aa58db14bb955aec16953336a
Suguru Yamamoto <sgryjp@gmail.com>
2019-03-30 00:10:23 +0900

    Code styling

--- test/positional.jl
+++ test/positional.jl
@@ -8,19 +8,19 @@ using CliOptions
 end
 
 @testset "consume(::Positional)" begin
-test_cases = [
-    ("file", "",      Vector{String}(), 1, AssertionError),
-    ("file", "",      [""],             1, (2, ("file" => "",))),
-    ("file", "",      ["-d"],           1, (2, ("file" => "-d",))),
-    ("file", "files", ["-d"],           1, (2, ("file" => "-d", "files" => "-d"))),
-]
-for (singular, plural, arg, index, expected) in test_cases
-    option = Positional(singular, plural)
-    ctx = Dict{Any,Int}(option => 0)
-    if expected isa Type && expected <: Exception
-        @test_throws expected CliOptions.consume!(ctx, option, arg, index)
-    else
-        @test CliOptions.consume!(ctx, option, arg, index) == expected
+    test_cases = [
+        ("file", "",      Vector{String}(), 1, AssertionError),
+        ("file", "",      [""],             1, (2, ("file" => "",))),
+        ("file", "",      ["-d"],           1, (2, ("file" => "-d",))),
+        ("file", "files", ["-d"],           1, (2, ("file" => "-d", "files" => "-d"))),
+    ]
+    for (singular, plural, arg, index, expected) in test_cases
+        option = Positional(singular, plural)
+        ctx = Dict{Any,Int}(option => 0)
+        if expected isa Type && expected <: Exception
+            @test_throws expected CliOptions.consume!(ctx, option, arg, index)
+        else
+            @test CliOptions.consume!(ctx, option, arg, index) == expected
+        end
     end
-end
 end
