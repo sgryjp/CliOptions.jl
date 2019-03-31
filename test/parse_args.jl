0cde1359fd1a81957e8051895dccc7d5f5b220dd
Suguru Yamamoto <sgryjp@gmail.com>
2019-03-31 19:32:42 +0900

    Add test cases

--- test/parse_args.jl
+++ test/parse_args.jl
@@ -5,18 +5,25 @@ using CliOptions
     @testset "Mixed options" begin
         options = (
             NamedOption("-n", "--num-workers"),
+            FlagOption("-i", "--ignore-case", negators=["--case-sensitive"]),
             Positional("filename"),
         )
         args = parse_args(options, ["-n", "3", "test.db"])
         @test args isa CliOptions.ParsedArguments
         @test args._dict["n"] == "3"
         @test args._dict["num_workers"] == "3"
+        @test args._dict["ignore_case"] == false
+        @test args._dict["case_sensitive"] == true
         @test args._dict["filename"] == "test.db"
         @test args["n"] == "3"
         @test args["num_workers"] == "3"
+        @test args["ignore_case"] == false
+        @test args["case_sensitive"] == true
         @test args["filename"] == "test.db"
         @test args.n == "3"
         @test args.num_workers == "3"
+        @test args.ignore_case == false
+        @test args.case_sensitive == true
         @test args.filename == "test.db"
 
         @test_throws CliOptionError parse_args(options, ["test.db", "test.txt"])
