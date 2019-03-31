10991c6b863e29464a08f03c16a639636fb1a719
Suguru Yamamoto <sgryjp@gmail.com>
2019-03-31 11:34:17 +0900

    Fix indentation

--- test/namedoption.jl
+++ test/namedoption.jl
@@ -9,23 +9,23 @@ using CliOptions
 end
 
 @testset "consume(::NamedOption)" begin
-names = ["-d", "--depth"]
-test_cases = [
-    (names, Vector{String}(), 1, AssertionError),
-    (names, [""],             1, (-1, nothing)),
-    (names, ["-a"],           1, (-1, nothing)),
-    (names, ["-d"],           1, CliOptionError),
-    (names, ["-d", "3"],      1, (3, ("d" => "3", "depth" => "3"))),
-    (names, ["a", "-d"],      2, CliOptionError),
-    (names, ["a", "-d", "3"], 2, (4, ("d" => "3", "depth" => "3"))),
-]
-for (names, arg, index, expected) in test_cases
-    option = NamedOption(names...)
-    ctx = Dict{Any,Int}(option => 0)
-    if expected isa Type && expected <: Exception
-        @test_throws expected CliOptions.consume!(ctx, option, arg, index)
-    else
-        @test CliOptions.consume!(ctx, option, arg, index) == expected
+    names = ["-d", "--depth"]
+    test_cases = [
+        (names, Vector{String}(), 1, AssertionError),
+        (names, [""],             1, (-1, nothing)),
+        (names, ["-a"],           1, (-1, nothing)),
+        (names, ["-d"],           1, CliOptionError),
+        (names, ["-d", "3"],      1, (3, ("d" => "3", "depth" => "3"))),
+        (names, ["a", "-d"],      2, CliOptionError),
+        (names, ["a", "-d", "3"], 2, (4, ("d" => "3", "depth" => "3"))),
+    ]
+    for (names, arg, index, expected) in test_cases
+        option = NamedOption(names...)
+        ctx = Dict{Any,Int}()
+        if expected isa Type && expected <: Exception
+            @test_throws expected CliOptions.consume!(ctx, option, arg, index)
+        else
+            @test CliOptions.consume!(ctx, option, arg, index) == expected
+        end
     end
-end
 end
