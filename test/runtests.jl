fc271be05e83f5ae5389ca04ad86976f423f233f
Suguru Yamamoto <sgryjp@gmail.com>
2019-05-01 14:42:55 +0900

    Make unit test target selectable

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,15 +3,32 @@ using CliOptions
 
 sorted_keys = sort ∘ collect ∘ keys
 
+tests = [
+    "internals.jl",
+    "parseresult.jl",
+    "option.jl",
+    "flagoption.jl",
+    "counteroption.jl",
+    "positional.jl",
+    "parse_args.jl",
+    "usage.jl",
+]
+targets = [lowercase(a) * ".jl" for a in ARGS]
+
+if "-h" in ARGS || "--help" in ARGS
+    println("Usage: julia \"$PROGRAM_FILE\" [TEST_NAME...]")
+    println()
+    println("Available Tests:")
+    for test_filename in tests
+        println("    " * test_filename[1:end-3])
+    end
+    exit()
+end
+
 @testset "CliOptions" begin
-
-    include("internals.jl")
-    include("parseresult.jl")
-    include("option.jl")
-    include("flagoption.jl")
-    include("counteroption.jl")
-    include("positional.jl")
-    include("parse_args.jl")
-    include("usage.jl")
-
+    for test_filename in tests
+        if length(targets) == 0 || test_filename in targets
+            include(test_filename)
+        end
+    end
 end
