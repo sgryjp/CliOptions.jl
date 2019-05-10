c0b345e078a3862ec4b160925101ae5feff5fe9c
Suguru Yamamoto <sgryjp@gmail.com>
2019-05-10 21:22:39 +0900

    Match test name with `startswith()`

--- test/runtests.jl
+++ test/runtests.jl
@@ -17,22 +17,33 @@ tests = [
     "parse_args.jl",
     "usage.jl",
 ]
-targets = [lowercase(a) * ".jl" for a in ARGS]
 
-if "-h" in ARGS || "--help" in ARGS
-    println("Usage: julia \"$PROGRAM_FILE\" [TEST_NAME...]")
-    println()
-    println("Available Tests:")
-    for test_filename in tests
-        println("    " * test_filename[1:end-3])
+targets = String[]
+let i = 1
+    while i ≤ length(ARGS)
+        if ARGS[i] == "-h" || ARGS[i] == "--help"
+            println("Usage: julia \"$PROGRAM_FILE\" [TEST_NAME...]")
+            println()
+            println("Available Tests:")
+            for test_filename in tests
+                println("    " * test_filename[1:end-3])
+            end
+            exit()
+        else
+            for t in tests
+                if startswith(t, ARGS[i])
+                    push!(targets, t)
+                end
+            end
+        end
+
+        i += 1
     end
-    exit()
+end
+if length(targets) == 0
+    append!(targets, tests)
 end
 
 @testset "CliOptions" begin
-    for test_filename in tests
-        if length(targets) == 0 || test_filename in targets
-            include(test_filename)
-        end
-    end
+    foreach(t -> include(t), targets)
 end
