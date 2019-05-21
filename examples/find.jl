5f257dba473d10fc6850dd80b638c55d5cf96637
Suguru Yamamoto <sgryjp@gmail.com>
2019-05-21 22:37:20 +0900

    Add find-like example

---
+++ examples/find.jl
@@ -0,0 +1,28 @@
+using CliOptions
+
+
+# A utility which searches for files of which name matches a given pattern and optionally
+# invoke a command line for each of them
+
+spec = CliOptionSpec(
+    HelpOption(),
+    Option("-x", "--exec"; default = missing, until = ";",
+           help = "Execute command for each found file."),
+    Positional("starting-point"; default = ".",
+               help = "Root of directory tree to execute recursive file search."),
+)
+options = parse_args(spec)
+
+# Recursively search for matching files
+for (rootpath, dirs, files) in walkdir(options.starting_point)
+    for fn in files
+        path = joinpath(rootpath, fn)
+        if ismissing(options.exec)
+            println(path)
+        else
+            commandline = copy(options.exec)
+            push!(commandline, path)
+            run(Cmd(commandline))
+        end
+    end
+end
